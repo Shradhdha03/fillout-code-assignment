8a828d041c7ee97452aebe6b4659853654ca7d14
Shradhdha Parsana <shradhdha03@gmail.com>
2024-03-08 22:46:56 -0500

    added home page setup

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const { filteredResponsesHandler } = require('./handlers/filteredResponsesHandle
 // API Routes
 const router = express.Router();
 router.get('/:formId/filteredResponses', filteredResponsesHandler);
+app.set('trust proxy', true);
+
+// Home page
+app.get('/', (req, res) => {
+    const protocol = req.protocol; // 'http' or 'https'
+    const host = req.get('host'); // Hostname or IP, including port if specified by the client
+    const serverUrl = `${protocol}://${host}/cLZojxk94ous/filteredResponses`
+    res.send(`<h3>Welcome to my Express application!<br/> Use url to get responses: <a href="${serverUrl}" >${serverUrl}</a><br/>- Shradhdha</h3>`);
+});
+
 app.use('/', router);
 
 app.listen(PORT, () => {
